3460957d3215ed550e48fd3cff1d5792451d83c3
Francois Campbell <campbell.francois@gmail.com>
2018-03-04 17:31:04 -0500

    move hoc

---
+++ src/components/WithZoom/index.tsx
@@ -0,0 +1,31 @@
+import * as React from 'react'
+
+
+export interface WithZoomProps {
+	children: (number) => new() => React.Component
+}
+
+export interface WithZoomState {
+	scaleFactor: number
+}
+
+export default class WithZoom extends React.PureComponent<WithZoomProps, WithZoomState> {
+	state = {
+		scaleFactor: 1
+	}
+
+	render() {
+		return (
+			<div onWheel={this.onMouseWheel}>
+				{this.props.children(this.state.scaleFactor)}
+			</div>
+		)
+	}
+
+	onMouseWheel = e => {
+		const scaleIncrement = e.deltaY / 300
+		this.setState({
+			scaleFactor: this.state.scaleFactor * (1 + scaleIncrement)
+		})
+	}
+}
